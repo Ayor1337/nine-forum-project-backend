f83d495624280dc8a1be5b92206bec5b6c3ea9bb
Ayor1337 <ayor1337@gmail.com>
2026-05-28 11:05:25 +0800

    web-app:  - 修改一些返回对象的字段

--- web/web-app/src/main/java/com/ayor/entity/vo/UserInfoVO.java
+++ web/web-app/src/main/java/com/ayor/entity/vo/UserInfoVO.java
@@ -1,6 +1,7 @@
 package com.ayor.entity.vo;
 
 import com.ayor.entity.vo.UserPermissionVO;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -36,5 +37,11 @@ public class UserInfoVO implements Serializable {
     private Date createTime;
 
     private Date updateTime;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean isFollowed;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Boolean isFollowing;
 
 }
